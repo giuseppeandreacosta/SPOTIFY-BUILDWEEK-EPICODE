0bec53919d0dfd1a1bf0016ff9219236de9198f2
Riccardo Spadon <spadon.riccardo@gmail.com>
2023-10-10 20:27:08 +0200

    Sistema barra ricerca

--- script.js
+++ script.js
@@ -1,14 +1,14 @@
 const searchIcon = document.querySelector(".o");
 const searchInput = document.querySelector("#search-input");
 
-searchIcon.addEventListener("click", () => {
+searchIcon.addEventListener('click', () => {
   searchInput.style.display = "block";
   searchIcon.style.display = "none";
   searchInput.focus();
 });
 
-searchInput.addEventListener("blur", () =>{
-    if(searchInput.value === "") {
+searchInput.addEventListener("blur", () => {
+    if(searchInput.value === '') {
        searchIcon.style.display = "block";
        searchInput.style.display = "none";
     }
