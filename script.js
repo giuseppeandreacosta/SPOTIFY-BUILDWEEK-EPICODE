952a1d315dc7a7eeee666df591895da7bd6dd190
Marshall1992sam <samu.co92@gmail.com>
2023-10-11 12:23:42 +0200

    sidebar
    aggiustare placeholder, la barra si abbassa quando si preme la lente

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const searchInput = document.querySelector("#search-input");
 searchIcon.addEventListener('click', () => {
   searchInput.style.display = "block";
   searchIcon.style.display = "none";
+  searchInput.style.width = "150px";
+  searchInput.style.border = "2px solid #81b71a";
   searchInput.focus();
 });
 
@@ -11,6 +13,7 @@ searchInput.addEventListener("blur", () => {
     if(searchInput.value === '') {
        searchIcon.style.display = "block";
        searchInput.style.display = "none";
+       searchInput.style.width = "0";
     }
 });
 
