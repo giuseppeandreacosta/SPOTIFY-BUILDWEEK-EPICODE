c031bf45341d7d2705e01a261a6b82581cde2285
Riccardo Spadon <spadon.riccardo@gmail.com>
2023-10-13 00:08:19 +0200

    changes

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 const searchIcon = document.querySelector(".o");
 const searchInput = document.querySelector("#search-input");
 
+const contenuto = document.querySelector("#artist-container .row")
+
+
 searchIcon.addEventListener('click', () => {
   searchInput.style.display = "block";
   searchIcon.style.display = "none";
@@ -10,10 +13,29 @@ searchIcon.addEventListener('click', () => {
 });
 
 searchInput.addEventListener("blur", () => {
-    if(searchInput.value === '') {
-       searchIcon.style.display = "block";
-       searchInput.style.display = "none";
-       searchInput.style.width = "0";
-    }
+  if (searchInput.value === '') {
+    searchIcon.style.display = "block";
+    searchInput.style.display = "none";
+    searchInput.style.width = "0";
+  }
 });
 
+
+
+const getResult = async () => {
+  try{
+    const response = await fetch(`https://striveschool-api.herokuapp.com/api/deezer/search?q=${query}`)
+    const data = await response.json()
+    return data
+  } catch (error){
+    console.log(error)
+  }
+}
+
+const displayResult = (result) => {
+  
+  
+  contenuto.innerHTML = result.slice(0, 14) /*html*/ `
+  
+  `
+}
