887a01f68a98e0717b2df0f42bfea2e8f244ef7f
Marshall1992sam <samu.co92@gmail.com>
2023-10-13 19:09:54 +0200

    manca poco..

--- script.js
+++ script.js
@@ -1,11 +1,9 @@
 const searchIcon = document.querySelector(".o");
 const searchInput = document.querySelector("#search-input");
 
-const contenuto = document.querySelector("#artist-container .row")
+const contenuto = document.querySelector("#artist-container .row");
 
-
-const contenuto = document.querySelector("#artist-container .row")
-
+const contenuto = document.querySelector("#artist-container .row");
 
 searchIcon.addEventListener("click", () => {
   searchInput.style.display = "block";
@@ -16,30 +14,31 @@ searchIcon.addEventListener("click", () => {
 });
 
 searchInput.addEventListener("blur", () => {
-  if (searchInput.value === '') {
+  if (searchInput.value === "") {
     searchIcon.style.display = "block";
     searchInput.style.display = "none";
     searchInput.style.width = "0";
   }
 });
 
-
 const getResult = async (query) => {
-  try{
-    const response = await fetch(`https://striveschool-api.herokuapp.com/api/deezer/search?q=${query}`)
-    const data = await response.json()
-    return data
-  } catch (error){
-    console.log(error)
+  try {
+    const response = await fetch(
+      `https://striveschool-api.herokuapp.com/api/deezer/search?q=${query}`
+    );
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.log(error);
   }
-}
+};
 
 const displayResult = (result, contenuto) => {
-  const songs = result.data
-  for(let i = 0; i < 1; i++) {
-    const song = songs[i]
-    console.log(song)
-      contenuto.innerHTML += /*html*/ `
+  const songs = result.data;
+  for (let i = 0; i < 1; i++) {
+    const song = songs[i];
+    console.log(song);
+    contenuto.innerHTML += /*html*/ `
       <div class="card col-md-2 p-3 align-items-center" id="${song.id}">
         <img src="${song.artist.picture_small}" class="card-img-top" alt="...">
         <div class="card-body">
@@ -47,19 +46,18 @@ const displayResult = (result, contenuto) => {
           <p class="card-text">${song.artist.type}</p>
         </div>
       </div>
-  `
+  `;
   }
-  
-}
+};
 
 const searchArtist = async () => {
-  const query = searchInput.value.trim()
-  
-  if(query.length > 0) {
-    const result = await getResult(query)
-    contenuto.innerHTML = ''
-    displayResult (result, contenuto)
+  const query = searchInput.value.trim();
+
+  if (query.length > 0) {
+    const result = await getResult(query);
+    contenuto.innerHTML = "";
+    displayResult(result, contenuto);
   } else {
-    contenuto.innerHTML = ''
+    contenuto.innerHTML = "";
   }
-}
+};
