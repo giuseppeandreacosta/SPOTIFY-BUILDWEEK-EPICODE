0e87a6a630006bd95ccf6d55d8b3b24dd69648ce
Riccardo Spadon <spadon.riccardo@gmail.com>
2023-10-13 17:23:47 +0200

    Changes

--- script.js
+++ script.js
@@ -22,7 +22,7 @@ searchInput.addEventListener("blur", () => {
 
 
 
-const getResult = async () => {
+const getResult = async (query) => {
   try{
     const response = await fetch(`https://striveschool-api.herokuapp.com/api/deezer/search?q=${query}`)
     const data = await response.json()
@@ -32,10 +32,32 @@ const getResult = async () => {
   }
 }
 
-const displayResult = (result) => {
-  
-  
-  contenuto.innerHTML = result.slice(0, 14) /*html*/ `
-  
+const displayResult = (result, contenuto) => {
+  const songs = result.data
+  for(let i = 0; i < 1; i++) {
+    const song = songs[i]
+    console.log(song)
+      contenuto.innerHTML += /*html*/ `
+      <div class="card col-md-2 p-3 align-items-center" id="${song.id}">
+        <img src="${song.artist.picture_small}" class="card-img-top" alt="...">
+        <div class="card-body">
+          <a href="../artist/artist.html?id=${song.artist.id}"><h6 class="card-text">${song.artist.name}</h6></a>
+          <p class="card-text">${song.artist.type}</p>
+        </div>
+      </div>
   `
+  }
+  
+}
+
+const searchArtist = async () => {
+  const query = searchInput.value.trim()
+  
+  if(query.length > 0) {
+    const result = await getResult(query)
+    contenuto.innerHTML = ''
+    displayResult (result, contenuto)
+  } else {
+    contenuto.innerHTML = ''
+  }
 }
